3c2340ee2f366fcb52e5d20e44c2f0373d066e3b
rssabbirdev <sabbirhossainrahad@gmail.com>
2024-07-02 21:04:31 +0400

    add back btn

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import { IoMdArrowBack } from 'react-icons/io';
 // eslint-disable-next-line react/prop-types
 export default function Navbar({ navMenuData = [] }) {
+	const goBack = () => {
+		window.history.back();
+	};
 	return (
 		<div className='flex justify-between items-center py-2 select-none px-3 sm:px-0'>
 			<div>
@@ -14,6 +18,12 @@ export default function Navbar({ navMenuData = [] }) {
 							{item?.name}
 						</li>
 					))}
+					{!navMenuData?.length && (
+						<IoMdArrowBack
+							onClick={() => goBack()}
+							className='text-4xl hover:cursor-pointer hover:text-gray-400 transition-all'
+						/>
+					)}
 				</ul>
 			</div>
 			<div>
